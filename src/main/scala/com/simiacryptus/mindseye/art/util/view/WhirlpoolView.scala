ccbfdfb160f47585a499d345fe1d45f3d3025c56
Andrew Charneski <acharneski@gmail.com>
2021-05-23 20:01:33 -0400

    Minor changes

--- src/main/scala/com/simiacryptus/mindseye/art/util/view/WhirlpoolView.scala
+++ src/main/scala/com/simiacryptus/mindseye/art/util/view/WhirlpoolView.scala
@@ -2,31 +2,42 @@ package com.simiacryptus.mindseye.art.util.view
 
 import com.simiacryptus.math.Point
 
-case class WhirlpoolView(step: Double = 0.8, seedSize: Double = 0.2, theta_step: Double = Math.PI / 16) extends IndexedView {
+case class WhirlpoolView(step: Double = 1.5, borderLocation: Double = 0.8, theta_step: Double = Math.PI / 16, trim : Double = 0.05) extends IndexedView {
   override def filterCircle = false
 
   def mappingFunction(): Point => Point = {
     (originalPoint: Point) => {
       try {
-        var point: Point = originalPoint
-        def inBounds(pt: Point) = {
-          pt.x.abs > seedSize || pt.y.abs > seedSize
-        }
-        var r = point.rms() * step
-        var theta = theta_step
-        var continue = true
-        while(continue) {
-          val newPoint = point.scale(r / point.rms()).rotate(theta)
-          if(inBounds(newPoint)) {
-            point = newPoint
-            r *= step
-            theta += theta_step
-          } else {
-            point = newPoint
-            continue = false;
+        if(1 - originalPoint.x.abs > ( 1 - trim)) {
+          new Point(0,0)
+        } else if(1 - originalPoint.y.abs > ( 1 - trim)) {
+          new Point(0,0)
+        } else {
+          var point: Point = originalPoint
+          def withinCenter(pt: Point) = {
+            (pt.x.abs < borderLocation && pt.y.abs < borderLocation)
           }
+          def outOfBounds(pt: Point) = {
+            (pt.x.abs >= 1 || pt.y.abs >= 1)
+          }
+          var r = point.rms() * step
+          var theta = theta_step
+          var continue = true
+          while(continue) {
+            val newPoint = point.scale(r / point.rms()).rotate(theta)
+            if(withinCenter(newPoint)) {
+              point = newPoint
+              r *= step
+              //theta += theta_step
+            } else {
+              if (!outOfBounds(newPoint)) {
+                point = newPoint
+              }
+              continue = false;
+            }
+          }
+          point
         }
-        point
       } catch {
         case e : Throwable => null
       }
